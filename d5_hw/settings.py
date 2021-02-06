f5650e9ddb04ea415d8c1c9fc5e98297f9348555
aleksioprime <57744848+aleksioprime@users.noreply.github.com>
2021-02-06 20:00:49 +0300

    edit

--- d5_hw/settings.py
+++ d5_hw/settings.py
@@ -15,16 +15,14 @@ from pathlib import Path
 import os
 
 # Строки для HEROKU
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Строки для HEROKU
 import django_heroku
 import dj_database_url  
 django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
-DISABLE_COLLECTSTATIC = 1
-# Строка для HEROKU
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
