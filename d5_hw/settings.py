a70dfb1fb79c103114f15b4f5bcf745c77eb723a
aleksioprime <57744848+aleksioprime@users.noreply.github.com>
2021-02-06 19:36:59 +0300

    edit setting

--- d5_hw/settings.py
+++ d5_hw/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'skillfactory-library-d5.herokuapp.com',
+]
 
 
 # Application definition
