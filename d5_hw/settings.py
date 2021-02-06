b883edea323a7585fc1c20e13bb26a8e4440efc4
aleksioprime <57744848+aleksioprime@users.noreply.github.com>
2021-02-06 19:56:56 +0300

    edit

--- d5_hw/settings.py
+++ d5_hw/settings.py
@@ -21,7 +21,7 @@ django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
-
+DISABLE_COLLECTSTATIC = 1
 # Строка для HEROKU
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -138,7 +138,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
 # Строки для Heroku
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
