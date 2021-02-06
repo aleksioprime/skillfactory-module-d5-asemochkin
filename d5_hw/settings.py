5c9813d6928d3cac83c777a8f38568ca2db6fc50
aleksioprime <57744848+aleksioprime@users.noreply.github.com>
2021-02-06 19:12:49 +0300

    edit env

--- d5_hw/settings.py
+++ d5_hw/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')3v0s^+)%n)hqpaz#$vnatk7dled0_i(p!rzbc(hvzb+nx48yo'
+# SECRET_KEY = ')3v0s^+)%n)hqpaz#$vnatk7dled0_i(p!rzbc(hvzb+nx48yo'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
